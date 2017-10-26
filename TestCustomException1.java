473afaf23c0d6734a6cd160e2eccdfeb2b9b88bf
gzpeterz <gzpeterz@163.com>
2017-10-26 13:33:06 +0800

    init

---
+++ TestCustomException1.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+class InvalidAgeException extends Exception{  
+ InvalidAgeException(String s){  
+  super(s);  
+ }  
+}  
+
+public class TestCustomException1{  
+  
+   static void validate(int age)throws InvalidAgeException{  
+     if(age<18)  
+      throw new InvalidAgeException("not valid");  
+     else  
+      System.out.println("welcome to vote");  
+   }  
+     
+   public static void main(String args[]){  
+      try{  
+      validate(13);  
+      }catch(Exception m){
+      	m.printStackTrace();
+      	System.out.println("Exception occured: "+m);
+      }  
+  
+      System.out.println("rest of the code...");  
+  }  
+}  
---
+++ Testtrycatch2.java
@@ -0,0 +1,13 @@
+public class Testtrycatch2{  
+  public static void main(String args[]){  
+   try{  
+      int data=50/0;  
+   }catch(ArithmeticException e){System.out.println(e);}  
+      /*
+   } finally {
+   		System.out.println("in finally now");
+   }
+   */
+   System.out.println("rest of the code...");  
+  }  
+}  
